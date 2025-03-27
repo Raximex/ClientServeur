3fa91c1a256cf18a63ecc3e9aa9566708585b164
LucaLanterne <luca.lanterne@gmail.com>
2025-03-27 11:44:45 +0100

    Client

--- Client/src/Client/Client.java
+++ Client/src/Client/Client.java
@@ -1,7 +1,25 @@
 package Client;
+import java.rmi.registry.LocateRegistry; 
+import java.rmi.registry.Registry;  
 
 public class Client {
-    //TEST
-    //TEST
-    //TEST
+    
+    private Client(){}
+
+    public static void main(String[] args) {
+        try {  
+            // Récupérer le registre
+            Registry reg = LocateRegistry.getRegistry(null); 
+       
+            // Recherche dans le registre de l'objet distant
+            IBricoMerlinServices stub = (IBricoMerlinServices) reg.lookup("IBricoMerlinServices"); 
+       
+            // Front client pour lancer les appels des méthodes distantes à l'aide de l'objet obtenu
+            //stub. 
+
+         } catch (Exception e) {
+            System.err.println(e.toString()); 
+            e.printStackTrace(); 
+         } 
+    }
 }
